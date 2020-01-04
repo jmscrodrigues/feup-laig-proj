b33e85ef16fd777b23057c9d4dc527177d50e5e4
MSR19 <miguel_silveira_rosa@hotmail.com>
2020-01-04 20:06:30 +0000

    bug fix in cylinder

--- MyCylinder.js
+++ MyCylinder.js
@@ -1,5 +1,5 @@
 /**
- * MyCylinder
+ * MyCilinder
  * @constructor
  * @param scene - Reference to MyScene object
  * @param base_radius  - Cylinder base radius
@@ -7,78 +7,119 @@
  * @param height - Cylinder height
  * @param slices - Number of sides in z axis
  * @param stacks - Number of divisons along the z axis 
+ * 
  */
 
 class MyCylinder extends CGFobject {
     constructor(scene,id, base_radius, top_radius, height, slices, stacks) {
         super(scene);
-            this.id = id;
-            this.scene = scene;
+        this.id = id;
 
-            if (height <= 0) height = 0.00001;
-            if (slices < 2) slices = 3;
+        if (height <= 0) height = 0.00001;
+        if (slices < 2) slices = 3;
 
-            this.base_radius = base_radius;
-            this.top_radius = top_radius;
-            this.height = height;
-            this.slices = slices;
-            this.stacks = stacks;
+        this.base_radius = base_radius;
+        this.top_radius = top_radius;
+        this.height = height;
+        this.slices = slices;
+        this.stacks = stacks;
 
-            this.degreeU = 3;
-            this.degreeV = 1;
-
-            this.halfCylinder;
-
-            this.getControlPoints();
-            this.makeSurface(scene);
-            
+        this.initBuffers();
     }
+    initBuffers() {
+        this.vertices = [];
+        this.indices = [];
+        this.normals = [];
+        this.texCoords = [];
 
-    getControlPoints() {
+        var ang = 0;
+        var alphaAng = 2*Math.PI/this.slices;
+        var heightStack = this.height/this.stacks;
+        var radiusAdjustment = (this.top_radius - this.base_radius)/this.stacks;
+        var actualHeight = 0;
+        var actualRadius = this.base_radius;
 
-        var xBase = this.base_radius;
-        var xTop = this.top_radius;
+        for (var t= 0; t<=this.stacks -1; t++) {
 
-        this.controlPoints = [];
 
-        this.controlPoints.push([xBase,0,0,1.0]);
-        this.controlPoints.push([xTop,0,this.height,1.0]);
-        this.controlPoints.push([xBase,(4/3) * xBase,0,1.0]);
-        this.controlPoints.push([xTop,(4/3) * xTop,this.height,1.0]);
+            for(var i = 0; i <= this.slices - 1; i++){
+                // All vertices have to be declared for a given face
+                // even if they are shared with others, as the normals 
+                // in each face will be different
+                
+                var seno = Math.sin(ang);
+                var cosseno= Math.cos(ang);
+               this.vertices.push(cosseno*(actualRadius+radiusAdjustment), seno*(actualRadius+radiusAdjustment),actualHeight + heightStack);
+               this.vertices.push(cosseno*actualRadius, seno*actualRadius,actualHeight);
+    
+    
+                // triangle normal computed by cross product of two edges
+                var normal= [
+                    Math.cos(ang),
+                    Math.sin(ang),
+                    0
+                ];
+    
+                // normalization
+                var nsize=Math.sqrt(
+                    normal[0]*normal[0]+
+                    normal[1]*normal[1]+
+                    normal[2]*normal[2]
+                    );
+                normal[0]/=nsize;
+                normal[1]/=nsize;
+                normal[2]/=nsize;
+    
+                // push normal once for each vertex of this triangle
+                this.normals.push(...normal);
+                this.normals.push(...normal);
+    
+                if (i == (this.slices - 1)) {
+                    this.indices.push(2*t*this.slices + (2*i) + 1, 2*t*this.slices + 1, 2*t*this.slices + (2*i));
+                    this.indices.push(2*t*this.slices + (2*i), 2*t*this.slices + 1, 2*t*this.slices);
+    
+                }
+                else {
+                    this.indices.push( 2*t*this.slices + (2*i) + 1, 2*t*this.slices + (2*i) + 3, 2*t*this.slices + (2*i));
+                    this.indices.push( 2*t*this.slices + (2*i), 2*t*this.slices + (2*i)+3, 2*t*this.slices + (2*i) + 2);
+                }
+                this.texCoords.push(i/(this.slices-1),1-(t+1)/this.stacks);
+                this.texCoords.push(i/(this.slices-1),1-t/this.stacks);
+                ang+=alphaAng;
+            }
 
-        this.controlPoints.push([-xBase,(4/3) * xBase,0,1.0]);
-        this.controlPoints.push([-xTop,(4/3) * xTop,this.height,1.0]);
-        this.controlPoints.push([-xBase,0,0,1.0]);
-        this.controlPoints.push([-xTop,0,this.height,1.0]);
-
-        this.finalControlPoints = [];
-
-        for (var i= 0; i <= this.degreeU; i++) {
-            var degreePoints = [];
-            for (var j = 0; j <= this.degreeV; j++)
-                degreePoints.push(this.controlPoints.shift());
-
-            this.finalControlPoints.push(degreePoints);
+            actualRadius += radiusAdjustment;
+            actualHeight += heightStack;
         }
 
+        this.primitiveType = this.scene.gl.TRIANGLES;
+        this.initGLBuffers();
     }
 
-    makeSurface(scene) {
-        var nurbsSurface = new CGFnurbsSurface(this.degreeU,this.degreeV, this.finalControlPoints);
+    /**
+	 * @method updateTexCoords
+	 * Updates the list of texture coordinates of the rectangle
+	 * @param {Array} coords - Array of texture coordinates
+	 */
+	updateTexCoords(coords) {
+		this.texCoords = [...coords];
+		this.updateTexCoordsGLBuffers();
+	}
 
+	/**
+	 * @method createTexCoords
+	 * creates the texcoords for the updateTexCoords method and call it
+	 * @param {Int} length_s - length of the texture/X on the x scale 
+	 * @param {Int} length_t - length of the textures/Y on the y scale 
+	 */
+	createTexCoords(length_s, length_t) {
+        var coords = [];
 
-        this.halfCylinder = new CGFnurbsObject(scene, this.slices, this.stacks, nurbsSurface);
+        for (var t= 0; t<=this.stacks -1; t++) {
+            for(var i = 0; i <= this.slices - 1; i++){
+                this.texCoords.push(i/(this.slices-1)/length_s,((t+1)/this.stacks)/length_t);
+                this.texCoords.push(i/(this.slices-1)/length_s,(t/this.stacks)/length_t);
+            }
+        }
     }
-
-    display() {
-        this.halfCylinder.display();
-        this.scene.pushMatrix();
-        this.scene.rotate(-Math.PI, 1,0,0);
-        this.scene.translate(0,0,-this.height);
-        this.halfCylinder.display();
-        this.scene.popMatrix();
-    }
-
-    createTexCoords() {};
-
-};
+}
